d559642d4aced658396cffaab1cd1e5bcb2163ac
Madhav Atri <dinglledad@Madhav.Madhav>
2026-06-30 11:18:12 +0530

    Implemented base.html,CSS and Admin Panel

--- home/views.py
+++ home/views.py
@@ -1,4 +1,18 @@
+from aiohttp import request
 from django.shortcuts import render , HttpResponse
 
 def index(request): 
-    return HttpResponse("This is Homepage")
+    context = {
+        'variable' : "This is sent from views.py"
+    }
+    return render(request, 'index.html', context)
+    
+    #return HttpResponse("This is Homepage")
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
